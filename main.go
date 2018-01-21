0529b500bbc5aed77cbeb9274f6dc8335e7a23aa
zhangxihe <egbertchang@icloud.com>
2018-01-21 23:09:24 +0800

    feat: pile up

--- main.go
+++ main.go
@@ -5,15 +5,16 @@ import (
   "net/http"
   "log"
   "time"
+  "AT_Message_Server/alephtau"
 )
 
 
 func main() {
   fmt.Println("Alephtau Server is starting")
-  
+
   server := &http.Server{
     Addr: ":9090",
-    Handler: &AlephTauServer{},    // type Handler interface {}，handler to invoke, http.DefaultServeMux if nil
+    Handler: &alephtau.Server{},    // type Handler interface {}，handler to invoke, http.DefaultServeMux if nil
     ReadTimeout: 3 * time.Minute,
     WriteTimeout: 3 * time.Minute,
     MaxHeaderBytes: 1 << 20,    // 这里依然使用右移的方式来表示存储空间
