2143720c5f1f27d8f7f9a24c35f5011e483b603f
egbertchang <egbertchang@icloud.com>
2018-01-21 16:23:41 +0800

    first commit

---
+++ at_server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "net/http"
+  "fmt"
+)
+
+type AlephTauServer struct {}
+
+
+func (s *AlephTauServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Println(r.URL.RawPath)
+}
+
