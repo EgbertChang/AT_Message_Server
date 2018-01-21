7cde9e169dd9f3f5260ab865f9d975e6e921608d
egbertchang <egbertchang@icloud.com>
2018-01-21 16:39:12 +0800

    feat: 更新.gitignore文件

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "log"
+  "time"
+)
+
+
+func main() {
+  fmt.Println("Alephtau Server is starting")
+  
+  server := &http.Server{
+    Addr: ":9090",
+    Handler: nil,    // type Handler interface {}，handler to invoke, http.DefaultServeMux if nil
+    ReadTimeout: 3 * time.Minute,
+    WriteTimeout: 3 * time.Minute,
+    MaxHeaderBytes: 1 << 20,    // 这里依然使用右移的方式来表示存储空间
+  }
+  log.Fatal(server.ListenAndServe())
+}
